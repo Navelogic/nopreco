f241a5f96378c8f05b1b2a7b143c5598a0549885
Arian Weslley <weslleysantoszbd@gmail.com>
2022-11-29 22:30:23 +0000

    Atualização V3

--- banco.sql
+++ banco.sql
@@ -2,25 +2,25 @@ CREATE DATABASE nopreco_db;
 USE nopreco_db;
 
 CREATE TABLE users (
-  id_user PRIMERY KEY AUTO_INCREMENT,
+  id_user INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(500) NOT NULL,
   email VARCHAR(500) NOT NULL,
   senha VARCHAR(500) NOT NULL,
   adicao INT NOT NULL DEFAULT 0,
-  verficado INT NOT NULL DEFAULT 0,
+  verficado INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE mercados (
-  id_mercado PRIMERY KEY AUTO_INCREMENT,
+  id_mercado INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(500) NOT NULL,
   classificacao INT NOT NULL DEFAULT 0,
   localizacao VARCHAR(500) NOT NULL,
   quantprodutos INT NOT NULL DEFAULT 0,
-  verficado INT NOT NULL DEFAULT 0,
+  verficado INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE produtos (
-  id_produto PRIMERY KEY AUTO_INCREMENT,
+  id_produto INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(500) NOT NULL,
   preco DECIMAL(5,2) NOT NULL,
   marca VARCHAR(500) NOT NULL,
@@ -29,10 +29,11 @@ CREATE TABLE produtos (
 );
 
 ALTER TABLE produtos
-
 ADD CONSTRAINT id_user
-
-ADD CONSTRAINT id_mercado
-
 FOREIGN KEY (id_user) REFERENCES users(id_user)
+
+
+
+ALTER TABLE produtos
+ADD CONSTRAINT id_mercado
 FOREIGN KEY (id_mercado) REFERENCES mercados(id_mercado)
