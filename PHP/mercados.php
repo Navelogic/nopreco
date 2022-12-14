30323f22485428efbbb5d06ebcf1765901ee5519
Arian Weslley <weslleysantoszbd@gmail.com>
2022-12-14 20:22:46 -0300

    Atualização Noturna

---
+++ PHP/mercados.php
@@ -0,0 +1,20 @@
+<?php
+include '../VIEW/header.html';
+include '../VIEW/navbar.html';
+?>
+
+<div class="container">
+  <div class="jumbotron jumbotron-fluid fonte">
+    <div class="container">
+      <h1 class="display-4">Todos os mercados cadastrados</h1>
+      <p class="lead">Note que esses são os verificados pela nossa equipe.</p>
+    </div>
+
+  <div class="card" style="width: 10rem;">
+    <img src="../IMG/extra-pin-branco.png" class="card-img-top" alt="...">
+</div>
+
+
+</div>
+
+</div>
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 include './VIEW/header.html';
 include './VIEW/navbar.html';
-
-include './VIEW/hero.html';
-include './VIEW/footer.html';
 ?>
