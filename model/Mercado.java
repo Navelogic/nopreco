9da85c89bd52048f7753b40f829e740830f7371d
Arian Weslley <weslleysantoszbd@gmail.com>
2022-12-20 00:31:20 -0300

    Criação de model Mercado e Produto

---
+++ model/Mercado.java
@@ -0,0 +1,14 @@
+public class Mercado {
+  private int id;
+  private String nomeMercado;
+  private String emailMercado;
+  private String enderecoMercado;
+  private String telefoneMercado;
+  private Produto id_produto;
+  
+
+
+
+
+  
+}
