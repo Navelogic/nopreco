540678e33eb8846d91ced544760ccc3f83c4df47
Arian Weslley <weslleysantoszbd@gmail.com>
2022-12-20 00:25:59 -0300

    Adição de model Pessoa

---
+++ model/Pessoa.java
@@ -0,0 +1,52 @@
+public class Pessoa {
+  private int id;
+  private String nomePessoa;
+  private String emailPessoa;
+  private String senhaPessoa;
+  private String enderecoPessoa;
+  private String cpf;
+  private String telefonePessoa;
+
+  public int getId() {
+    return id;
+  };
+  public void setId(int id) {
+    this.id = id;
+  };
+  public String getNomePessoa() {
+    return nomePessoa;
+  }
+  public void setNomePessoa(String nomePessoa){
+    this.nomePessoa = nomePessoa;
+  }
+  public String getEmailPessoa() {
+    return emailPessoa;
+  }
+  public void setEmailPessoa(String emailPessoa){
+    this.emailPessoa = emailPessoa;
+  }
+  public String getsenhaPessoa(){
+    return senhaPessoa;
+  }
+  public void setsenhaPessoa(String senhaPessoa){
+    this.senhaPessoa = senhaPessoa;
+  }
+  public String getEnderecoPessoa(){
+    return enderecoPessoa;
+  }
+  public void setEnderecoPessoa(String enderecoPessoa){
+    this.enderecoPessoa = enderecoPessoa;
+  }
+  public String getCpf(){
+    return cpf;
+  }
+  public void setCpf(String cpf){
+    this.cpf = cpf;
+  }
+  public String getTelefonePessoa(){
+    return telefonePessoa;
+  }
+  public void setTelefonePessoa(String telefonePessoa){
+    this.telefonePessoa = telefonePessoa;
+  }
+}
