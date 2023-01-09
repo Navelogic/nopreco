3028e447e7b2427a98498af614783c1d0a917482
Henrique Padilha <93350805+Navelogic@users.noreply.github.com>
2023-01-09 09:26:11 -0300

    Algumas Atualizações

---
+++ SQL/carga.sql
@@ -0,0 +1,5 @@
+USE nopreco_db;
+
+
+INSERT INTO table_name (column1, column2, column3, ...)
+INSERT INTO users (nome, email, senha, cpf, adicao, verficado) VALUES (null, 'Arian Weslley', 'weslleysantoszbd@gmail.com', 'teste@teste', '00000000000');
